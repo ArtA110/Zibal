2095818904b7e0c1995791c1bd26ca59bb05b4e5
Alireza Tabatabaeian <ali.tabatabaeian16@gmail.com>
2025-04-19 12:31:33 +0330

    Refact: Delete extra imports

--- core/report/views.py
+++ core/report/views.py
@@ -1,9 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
-from django.conf import settings
 from core.mongo_client import db
-from datetime import datetime
 import jdatetime
 from bson import ObjectId
 
